4c76ed2d808997f50410f4d91a19806a5db1fad7
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2023-09-11 13:20:38 +0700

    finishing 2.8 and 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const filteredPersons =
+    searchKeyword !== ""
+      ? persons.filter((person) => person.name.includes(searchKeyword))
+      : persons;
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const compareObject = (first, second) => {
@@ -30,31 +46,42 @@ const App = () => {
     event.preventDefault();
     const newObject = {
       name: newName,
+      number: newNumber,
     };
 
     if (persons.some((person) => compareObject(person, newObject))) {
+      // down side is when the number is same this alert still will be displayed
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons([...persons, newObject]);
       setNewName("");
+      setNewNumber("");
     }
-
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={(e)=>setSearchKeyword(e.target.value)}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPersonHandler}>
         <div>
           name: <input onChange={handleNewName} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNewNumber} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <div key={i}>{person.name}</div>
+      {filteredPersons.map((person, i) => (
+        <div key={i}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
